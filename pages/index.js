63b6771b3d694544007266591a0639536b0491c1
Benjamin Shafii <ben@prologe.io>
2022-11-21 17:05:19 +0100

    feat: add spinner

--- pages/index.js
+++ pages/index.js
@@ -101,6 +101,32 @@ const MOCK_QUESTIONS = [
   "When was the last time you felt like you were in the zone?",
   "What is the most important thing you learned in the last year?",
 ];
+
+const Spinner = () => {
+  // if (true) return <div>yo</div>
+  return (
+    <svg
+      class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        class="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        stroke-width="4"
+      ></circle>
+      <path
+        class="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      ></path>
+    </svg>
+  );
+};
 const QuestionGenerator = ({ children }) => {
   const [question, setQuestion] = useState(
     "What do you think about Hackers & Painters from Paul Graham?"
@@ -110,13 +136,13 @@ const QuestionGenerator = ({ children }) => {
     register,
     handleSubmit,
     watch,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm();
 
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     const { topics } = data;
     const topicsArray = topics.split(",").map((t) => t.trim());
-    getQuestion(topicsArray);
+    await getQuestion(topicsArray);
   };
 
   const getQuestion = async (topics) => {
@@ -133,7 +159,12 @@ const QuestionGenerator = ({ children }) => {
   return (
     <div className="flex flex-col gap-3">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <PrimaryButton type="submit" className="mb-3">
+        <PrimaryButton
+          type="submit"
+          className="mb-3 disabled:opacity-75"
+          disabled={isSubmitting}
+        >
+          {isSubmitting && <Spinner />}
           🤖 Press to generate question about
         </PrimaryButton>
         <Input
