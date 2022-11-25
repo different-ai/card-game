3288e7a326454fc2dabc1d0c24f023a0a4e3bd65
Benjamin Shafii <ben@prologe.io>
2022-11-25 17:23:08 +0100

    feat: add posthog init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { usePostHog } from 'next-use-posthog'
-
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { usePostHog } from "next-use-posthog";
+import posthog from "posthog-js";
 
 export default function App({ Component, pageProps }: AppProps) {
-  usePostHog('phc_XGOSiimQQpEK9TDZN7NZ6wkfy5gp81DNPDR4OLllda3', {
-    api_host: 'https://app.posthog.com',
-    loaded: (posthog) => {
-      if (process.env.NODE_ENV === 'development') posthog.opt_out_capturing()
-    },
-  })  
+  posthog.init("phc_XGOSiimQQpEK9TDZN7NZ6wkfy5gp81DNPDR4OLllda3", {
+    api_host: "https://app.posthog.com",
+  });
 
-  return <Component {...pageProps} />
+  usePostHog("phc_XGOSiimQQpEK9TDZN7NZ6wkfy5gp81DNPDR4OLllda3", {
+    api_host: "https://app.posthog.com",
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.opt_out_capturing();
+    },
+  });
+
+  return <Component {...pageProps} />;
 }
