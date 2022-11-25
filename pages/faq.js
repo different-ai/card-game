8228d3c38776c13089dd52e257fa3e412fcf41fd
Benjamin Shafii <ben@prologe.io>
2022-11-25 17:15:25 +0100

    feat: add faq

---
+++ pages/faq.js
@@ -0,0 +1,59 @@
+import { Header } from ".";
+
+const faqs = [
+  {
+    question: "How do I play?",
+    answer: "Sit in a small group, pick a card and take turns answering",
+  },
+  {
+    question: "How do I create my own deck?",
+    answer:
+      "Swipe left or right to select or deselect a card. Swiping right puts the card at the end of your deck, swiping left removes it from your deck.",
+  },
+  {
+    question: "How much a deck costs?",
+    answer: "$25",
+  },
+];
+
+export default function Example() {
+  return (
+    <div className="bg-white">
+      <div className="relative pt-6 pb-16 sm:pb-24 lg:pb-32">
+        <Header />
+        <div className="mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:py-20 lg:px-8">
+          <div className="lg:grid lg:grid-cols-3 lg:gap-8">
+            <div>
+              <h2 className="text-3xl font-bold tracking-tight text-gray-900">
+                Frequently asked questions
+              </h2>
+              <p className="mt-4 text-lg text-gray-500">
+                Can’t find the answer you’re looking for? Reach out to{" "}
+                <a
+                  href="mailto:contact@langa.me"
+                  className="font-medium text-indigo-600 hover:text-indigo-500"
+                >
+                  us
+                </a>
+              </p>
+            </div>
+            <div className="mt-12 lg:col-span-2 lg:mt-0">
+              <dl className="space-y-12">
+                {faqs.map((faq) => (
+                  <div key={faq.question}>
+                    <dt className="text-lg font-medium leading-6 text-gray-900">
+                      {faq.question}
+                    </dt>
+                    <dd className="mt-2 text-base text-gray-500">
+                      {faq.answer}
+                    </dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,9 @@ import { QuestionGenerator } from "../components/QuestionGenerator";
 import CardList from "../components/CardList";
 import { useQuestionStore } from "../store";
 import CheckoutForm from "../components/CheckoutForm";
+import Link from "next/link";
 
-const navigation = [
-  { name: "Product", href: "#" },
-  { name: "Features", href: "#" },
-];
+const navigation = [{ name: "FAQ", href: "/faq" }];
 
 const Pattern = () => {
   return (
@@ -113,82 +111,90 @@ const CardCounter = () => {
     </div>
   );
 };
-
-export default function Home() {
+export const Header = () => {
   return (
-    <div className="relative overflow-hidden bg-white">
-      <Pattern2 />
-      <div className="relative pt-6 pb-16 sm:pb-24 lg:pb-32">
-        <Popover>
-          <nav
-            className="relative mx-auto flex max-w-7xl items-center justify-between px-4 sm:px-6"
-            aria-label="Global"
-          >
-            <div className="flex flex-1 items-center">
-              <div className="flex w-full items-center justify-between md:w-auto">
-                <a href="#">
-                  <span className="sr-only">Your Company</span>
+    <>
+      <Popover>
+        <nav
+          className="relative mx-auto flex max-w-7xl items-center justify-between px-4 sm:px-6"
+          aria-label="Global"
+        >
+          <div className="flex flex-1 items-center">
+            <div className="flex w-full items-center justify-between md:w-auto">
+              <Link href="/">
+                  <span className="sr-only">Lang Game</span>
                   <LangameIcon />
+              </Link>
+              <div className="-mr-2 flex items-center md:hidden">
+                <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <span className="sr-only">Open main menu</span>
+                  <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                </Popover.Button>
+              </div>
+            </div>
+            <div className="hidden md:ml-10 md:block md:space-x-10">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className="font-medium text-gray-500 hover:text-gray-900"
+                >
+                  {item.name}
                 </a>
-                <div className="-mr-2 flex items-center md:hidden">
+              ))}
+            </div>
+          </div>
+        </nav>
+
+        <Transition
+          as={Fragment}
+          enter="duration-150 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-100 ease-in"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <Popover.Panel
+            focus
+            className="absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden"
+          >
+            <div className="overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5">
+              <div className="flex items-center justify-between px-5 pt-4">
+                <div></div>
+                <div className="-mr-2">
                   <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                    <span className="sr-only">Open main menu</span>
-                    <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+                    <span className="sr-only">Close main menu</span>
+                    <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                   </Popover.Button>
                 </div>
               </div>
-              <div className="hidden md:ml-10 md:block md:space-x-10">
+              <div className="space-y-1 px-2 pt-2 pb-3">
                 {navigation.map((item) => (
                   <a
                     key={item.name}
                     href={item.href}
-                    className="font-medium text-gray-500 hover:text-gray-900"
+                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900"
                   >
                     {item.name}
                   </a>
                 ))}
               </div>
             </div>
-          </nav>
+          </Popover.Panel>
+        </Transition>
+      </Popover>
+    </>
+  );
+};
 
-          <Transition
-            as={Fragment}
-            enter="duration-150 ease-out"
-            enterFrom="opacity-0 scale-95"
-            enterTo="opacity-100 scale-100"
-            leave="duration-100 ease-in"
-            leaveFrom="opacity-100 scale-100"
-            leaveTo="opacity-0 scale-95"
-          >
-            <Popover.Panel
-              focus
-              className="absolute inset-x-0 top-0 z-10 origin-top-right transform p-2 transition md:hidden"
-            >
-              <div className="overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5">
-                <div className="flex items-center justify-between px-5 pt-4">
-                  <div></div>
-                  <div className="-mr-2">
-                    <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                      <span className="sr-only">Close main menu</span>
-                      <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                    </Popover.Button>
-                  </div>
-                </div>
-                <div className="space-y-1 px-2 pt-2 pb-3">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-gray-50 hover:text-gray-900"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
-                </div>
-              </div>
-            </Popover.Panel>
-          </Transition>
-        </Popover>
+export default function Home() {
+  return (
+    <div className="relative overflow-hidden bg-white">
+      <Pattern2 />
+
+      <div className="relative pt-6 pb-16 sm:pb-24 lg:pb-32">
+        <Header />
 
         <main className="mx-auto mt-16 max-w-7xl px-4 sm:mt-24 sm:px-6 lg:mt-32">
           <div className="lg:grid lg:grid-cols-12 lg:gap-8">
