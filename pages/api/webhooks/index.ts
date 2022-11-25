b0f5da4f0e8c8080b6c055f38384ecb41e286596
Benjamin Shafii <ben@prologe.io>
2022-11-25 16:10:44 +0100

    fix: type issues

--- pages/api/webhooks/index.ts
+++ pages/api/webhooks/index.ts
@@ -5,7 +5,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import Stripe from 'stripe'
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
   // https://github.com/stripe/stripe-node#configuration
-  apiVersion: '2020-08-27',
+  apiVersion: '2022-08-01',
 })
 
 const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET!
