7c5244c092197d220a4edbb3b77fbf65e57bfad9
Benjamin Shafii <ben@prologe.io>
2022-11-25 17:19:35 +0100

    feat: add posthog

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { usePostHog } from 'next-use-posthog'
+
 
 export default function App({ Component, pageProps }: AppProps) {
+  usePostHog('phc_XGOSiimQQpEK9TDZN7NZ6wkfy5gp81DNPDR4OLllda3', {
+    api_host: 'https://app.posthog.com',
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === 'development') posthog.opt_out_capturing()
+    },
+  })  
+
   return <Component {...pageProps} />
 }
