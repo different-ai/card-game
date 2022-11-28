998d5c84fb0ea6ff5bd970f9cd3c8168a5a17b21
Benjamin Shafii <ben@prologe.io>
2022-11-28 11:16:39 +0100

    feat: output 5 questions

--- pages/api/questions.ts
+++ pages/api/questions.ts
@@ -14,12 +14,10 @@ export default async function handler(
   console.log(API_KEY);
   console.log("hit questions");
 
-
-
   const response = await axios.post(
     "https://api.langa.me/v1/conversation/starter",
-    { topics: req.body.topics, limit: 3 },
-    { headers: { "X-Api-Key": API_KEY, 'accept-encoding': '*' } }
+    { topics: req.body.topics, limit: 5 },
+    { headers: { "X-Api-Key": API_KEY, "accept-encoding": "*" } }
   );
   const questions = response.data.results;
   console.log(questions, response.data);
